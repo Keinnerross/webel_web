bdb6368fb353e9e06e7cce2c143b5ce6ee024bd0
keinnerross <keinnerross@gmail.com>
2024-06-01 19:36:14 -0400

    update

--- src/app/Layout/header.jsx
+++ src/app/Layout/header.jsx
@@ -1,8 +1,19 @@
 const Header = () => {
     return (
         <div className="w-full bg-slate-900 h-16 flex items-center justify-center">
-            <div className="w-3/4 h-12 text-white">
-                <p>hello world</p>
+            <div className="w-3/4  text-white flex justify-between align-center ">
+                <div className="flex gap-24" >
+                    <span>Webel Chile</span>
+                    <nav>
+                        <ul className="flex gap-6">
+                            <li className="cursor-pointer">Servicios</li>
+                            <li className="cursor-pointer">Trabajos</li>
+                            <li className="cursor-pointer">Reviews</li>
+                            <li className="cursor-pointer">Sobre Nosotros</li>
+                        </ul>
+                    </nav>
+                </div>
+                <B
             </div>
         </div>
 
